3a61009eac189e870449bba5f368069c45a0979f
Maciej Błędkowski <23295125+mbledkowski@users.noreply.github.com>
2021-10-20 21:41:35 +0200

    Added vue.config.js with vue-cli-pwa config, and added favicons.

---
+++ vue.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  pwa: {
+    name: 'keycomp',
+    themeColor: '#000000',
+    msTileColor: '#000000',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
+    iconPaths: {
+      faviconSVG: 'img/icons/favicon.svg',
+      favicon32: 'img/icons/favicon-32x32.png',
+      favicon16: 'img/icons/favicon-16x16.png',
+      appleTouchIcon: 'img/icons/apple-touch-icon.png',
+      maskIcon: 'img/icons/safari-pinned-tab.svg',
+      msTileImage: 'img/icons/msapplication-icon-144x144.png',
+    },
+  },
+}
