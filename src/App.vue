505c0e153161dee7e045995363333dde38d53339
Maciej Błędkowski <23295125+mbledkowski@users.noreply.github.com>
2021-10-25 19:44:25 +0200

    Added footer to App.vue

--- src/App.vue
+++ src/App.vue
@@ -11,8 +11,24 @@ header
     //-     :to='category.dir'
     //-   ) {{ category.name }}
 router-view
+footer
+  .container
+    span [
+      |
+      |
+      a(href="https://github.com/mbledkowski/keycomp")
+        | GitHub
+      |  ]
+    span
+      | [
+      |
+      a(href="https://github.com/mbledkowski/keycomp/blob/main/LICENSE")
+        | License:
+        | GPL-3.0
+      |  ]
 </template>
 <script lang="ts">
+// Category Template
 class CatTemplate {
   name: string;
 
@@ -61,20 +77,27 @@ export default {
   --border-dark: #7A7A7A;
   --background-dark: #0a0a0f;
 
-  --pico-spacing: 8px;
-  --nano-spacing: 16px;
-  --mili-spacing: 24px;
-  --normal-spacing: 32px;
-  --kilo-spacing: 40px;
-  --giga-spacing: 56px;
+  // --pico-spacing: clamp(4px, 0.67vw, 8px);
+  // --nano-spacing: clamp(8px, 1.33vw, 16px);
+  // --mili-spacing: clamp(12px, 2vw, 24px);
+  // --normal-spacing: clamp(16px, 2.67vw, 32px);
+  // --kilo-spacing: clamp(20px, 3.33vw, 40px);
+  // --giga-spacing: clamp(28px, 4.67vw, 56px);
+
+  --pico-spacing: clamp(6px, 0.67vw, 8px);
+  --nano-spacing: clamp(12px, 1.33vw, 16px);
+  --mili-spacing: clamp(18px, 2vw, 24px);
+  --normal-spacing: clamp(24px, 2.67vw, 32px);
+  --kilo-spacing: clamp(30px, 3.33vw, 40px);
+  --giga-spacing: clamp(48px, 4.67vw, 56px);
 
   --font: "Barlow";
 
-  --button: 500 16px var(--font);
-  --title3-bold: 600 20px var(--font);
-  --title3: normal 20px var(--font);
-  --title2: normal 22px var(--font);
-  --xlargetitle: normal 54px var(--font);
+  --button: 500 clamp(12px, 1.33vw, 16px) var(--font);
+  --title3-bold: 600 clamp(15px, 1.67vw, 20px) var(--font);
+  --title3: normal clamp(15px, 1.67vw, 20px) var(--font);
+  --title2: normal clamp(16.5px, 1.83vw, 22px) var(--font);
+  --xlargetitle: normal clamp(40.5px, 4.5vw, 54px) var(--font);
 }
 body {
   background: var(--background);
@@ -91,54 +114,64 @@ header {
   // top:0;
   z-index: 144;
   > .container {
-    display: flex;
-    margin: auto;
-    width: 100%;
-    max-width: 1920px;
-  > a {
-    display: flex;
-    flex-flow: row nowrap;
-    > img {
-    margin: auto auto auto var(--mili-spacing);
-    filter: invert(1);
-    }
-    > .version {
-      position: relative;
-      height: fit-content;
-      top: 12px;
-      left: 6px;
-      color: var(--background);
-      background: var(--foreground);
-      padding: 0 4px;
-      border-radius: 4px;
-      font-size: 12px;
-    }
-  }
-  > *:not(:last-child) {
-    margin-right: auto;
-  }
-  > nav {
-    padding: 4px;
-    border: var(--accent) solid;
-    border-radius: 14px;
-    > ul {
-      list-style-type: none;
-      margin: 0;
-      padding: 0;
+      display: flex;
+      margin: auto;
+      width: 100%;
+      max-width: 1920px;
+    > a {
       display: flex;
       flex-flow: row nowrap;
-    }
-    a {
-      padding: 4px 6px;
-      font-size: 16px;
-      font-weight: bold;
-      color: white;
-      text-decoration: none;
-      border-radius: 8px;
-      &.router-link-exact-active {
-        background: var(--accent);
+      > img {
+      margin: auto auto auto var(--mili-spacing);
+      filter: invert(1);
       }
-    }  }
+      > .version {
+        position: relative;
+        height: fit-content;
+        top: 12px;
+        left: 6px;
+        color: var(--background);
+        background: var(--foreground);
+        padding: 0 4px;
+        border-radius: 4px;
+        font-size: 12px;
+      }
+    }
+    > *:not(:last-child) {
+      margin-right: auto;
+    }
+    > nav {
+      padding: 4px;
+      border: var(--accent) solid;
+      border-radius: 14px;
+      > ul {
+        list-style-type: none;
+        margin: 0;
+        padding: 0;
+        display: flex;
+        flex-flow: row nowrap;
+      }
+      a {
+        padding: 4px 6px;
+        font-size: 16px;
+        font-weight: bold;
+        color: white;
+        text-decoration: none;
+        border-radius: 8px;
+        &.router-link-exact-active {
+          background: var(--accent);
+        }
+    }}
+  }
+}
+
+footer > .container {
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: center;
+  font: var(--button);
+  > span{
+    margin: var(--nano-spacing);
   }
 }
 @media (prefers-color-scheme: dark) {
